79345090bd1da2b7fb61e1dcefe4657aebc1a96b
Chris <398808059@qq.com>
2023-01-25 14:57:32 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { reactive , ref} from 'vue'
+import { useRouter } from 'vue-router';
+const state = reactive({ count: 0 })
+const router = useRouter()
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+function regist(event) {
+  if(event){
+    console.log("----",event)
+    router.push({ path: '/Regist' })
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+    <div @click = "regist('regist')">注册</div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/views/Regist.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="hello">
+    <div class="box">
+      <div class="f2" style = 'width: 300px;height: 300px;border: 1px solid salmon;'>
+        <div class="box">
+          <div class="f1" >昵称</div>
+          <el-input class="f4" 
+          v-model="username" placeholder="请输入内容"></el-input>
+        </div>
+        <div class="box">
+          <div class="f1">密码</div>
+          <el-input class="f4" v-model="password" placeholder="请输入内容"></el-input>
+        </div>
+        <div >
+          <el-button @click = "regist('regist')">注册</el-button>
+          <el-button @click = "regist('login')">登陆</el-button>
+        </div>
+      </div>
+      <div class="f8">注册页面</div>
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Regist',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      username:'',
+      password:'',
+      tables:[
+        
+      ]
+    }
+  },
+  created(){
+for(let i =0 ;i<=10;i++){
+  let ddd = []
+  for(let j =0 ;j<=10;j++){
+    ddd.push(
+      {
+        id:j,
+        hang:`第${j}行`
+      }
+    )
+
+  }
+  this.tables.push(
+    {
+        id:i,
+        ind:ddd
+      } 
+    )
+
+}
+console.log(' this.tables', this.tables)
+  },
+  methods:{
+    regist(type){
+      let url
+      if(type =='regist'){
+        url = '/user/regist'
+      }else{
+        url = '/user/login'
+      }
+      this.axios.post(url, {
+        name: this.username,
+        password: this.password,
+      })
+      // this.$router.push({ path: '/foo' })
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
